1700f29f8db5d5a196300b82faf07e33c2a11173
DESKTOP-R55DTSI\Atheeksh <abhagavath225@student.fuhsd.org>
2021-07-17 23:12:58 -0700

    Working on adding pagination and sorting to the webserver

--- src/main/java/com/jed/studentinfo/AccessingDataMysqlApplication.java
+++ src/main/java/com/jed/studentinfo/AccessingDataMysqlApplication.java
@@ -1,13 +1,25 @@
 package com.jed.studentinfo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessingDataMysqlApplication {
+public class AccessingDataMysqlApplication implements CommandLineRunner {
 
+    @Autowired
+    UserRepository userRepo;
     public static void main(String[] args) {
         SpringApplication.run(AccessingDataMysqlApplication.class, args);
     }
-
+    @Override
+    public void run(String... arg0) throws Exception {
+        for(User user : userRepo.search("a")) {
+            System.out.println("Id: " + user.getId());
+            System.out.println("Name: " + user.getName());
+            System.out.println("Email: "+ user.getEmail());
+            System.out.println("================================");
+        }
+    }
 }
--- src/main/java/com/jed/studentinfo/UserRepository.java
+++ src/main/java/com/jed/studentinfo/UserRepository.java
@@ -1,10 +1,15 @@
 package com.jed.studentinfo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    @Query("from User where name like %:keyword%")
+    List<User> search(@Param("keyword") String keyword);
 }
